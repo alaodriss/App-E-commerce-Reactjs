8462451556a4ea07786be373b0b656b964481318
alaodriss <alaodriss@gmail.com>
2023-03-06 11:23:31 +0100

    Projet ecommerce with reactjs ... [this part is Header]

---
+++ src/components/Header.js
@@ -0,0 +1,93 @@
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import { BsSearch } from "react-icons/bs";
+const Header = () => {
+  return (
+    <>
+      <header className="header-top-strip py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <p className="text-white mb-0">
+                Free Shipping Over $100 @ Free Returns
+              </p>
+            </div>
+            <div className="col-6">
+              <p className="text-end text-white mb-0">
+                Hotline:{" "}
+                <a className="text-white" href="Tel: +330712345667">
+                 
+                  +33.07.12.34.56
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </header>
+      <header className="header-upper py-3">
+        <div className="container-xxl">
+          <div className="row aglin-items-center">
+            <div className="col-2">
+              <h2>
+                <Link className="text-white">Developpe</Link>
+              </h2>
+            </div>
+            <div className="col-5">
+              <div class="input-group mb-3">
+                <input
+                  type="text"
+                  className="form-control py-2"
+                  placeholder="Search Product Here..."
+                  aria-label="Search Product Here..."
+                  aria-describedby="basic-addon2"
+                />
+                <span className="input-group-text p-3" id="basic-addon2">
+                  <BsSearch className="fs-6" />
+                </span>
+              </div>
+            </div>
+            <div className="col-5">
+              <div className="header-upper-links d-flex align-items-center justify-content-between">
+                <div>
+                  <Link className="d-flex align-items center gap-10 text-white">
+                    <img src="images/compare.svg" alt="compare" />
+                    <p>
+                      Compare <br /> Products
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className="d-flex align-items center gap-10 text-white">
+                    <img src="images/wishlist.svg" alt="wishlist" />
+                    <p>
+                      Favourite <br /> Wishlist
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className="d-flex align-items center gap-10 text-white">
+                    <img src="images/user.svg" alt="user" />
+                    <p>
+                      Login <br /> My Account
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className="d-flex align-items center gap-10 text-white">
+                    <img src="images/cart.svg" alt="cart" />
+                    <div className="d-flex flex-column gap-10">
+                      <span className="badge bg-white text-dark">0</span>
+                      <p> $ 500</p>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
